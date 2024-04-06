df32038d6d014c6387ebeb4a7506aac7e422de7f
paytondev <81662879+paytontech@users.noreply.github.com>
2024-04-06 12:55:31 -0400

    Initial commit

---
+++ Sources/SharedQSync/SharedQSync.swift
@@ -0,0 +1,105 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import Foundation
+
+public class SharedQSyncManager {
+    var delegate: SharedQSyncDelegate?
+    var serverURL: URL
+    var websocketURL: URL
+    var socket: URLSessionWebSocketTask?
+    public init(serverURL: URL = URL(string: "http://sq.paytondev.cloud:8080")!, websocketURL: URL = URL(string: "ws://sq.paytondev.cloud:8080")!) {
+        self.serverURL = serverURL
+        self.websocketURL = websocketURL
+    }
+    /// Creates a WebSocket session with the server (uses websocketURL from `init`)
+    public func connectToGroup(group: SQGroup, user: SQUser) {
+        if delegate == nil {
+            print("[SharedQSync] [WARNING] No delegate has been provided for this instance of SharedQSyncManager. You will not recieve any messages from the server.")
+        }
+//        let socketURL = URL(string: "\(baseWSURL)/group/\(self.currentUser!.id)/\(group.id)")!
+        let socketURL = websocketURL.appending(path: "/group/\(user.id)/\(group.id)")
+        let session = URLSession(configuration: .ephemeral)
+        self.socket = session.webSocketTask(with: socketURL)
+        socket!.resume()
+        socket!.receive { res in
+            switch res {
+            case .success(let res2):
+                switch res2 {
+                case .data(let data):
+                    let wsMessage = try! JSONDecoder().decode(WSMessage.self, from: data)
+                    switch wsMessage.type {
+                    case .groupUpdate:
+                        let groupJSON = try! JSONDecoder().decode(SQGroup.self, from: wsMessage.data)
+                        if groupJSON.playbackState?.state == .pause {
+                            if let delegate = self.delegate {
+                                delegate.onPause(wsMessage)
+                            }
+                        }
+                            if let delegate = self.delegate {
+                                delegate.onGroupUpdate(groupJSON, wsMessage)
+                            }
+                    case .timestampUpdate:
+                        
+                            let timestampUpdateInfo = try! JSONDecoder().decode(WSTimestampUpdate.self, from: wsMessage.data)
+                        if let delegate = self.delegate {
+                            delegate.onTimestampUpdate(timestampUpdateInfo.timestamp, wsMessage)
+                        }
+                    case .nextSong:
+                        if let delegate = self.delegate {
+                            delegate.onNextSong(wsMessage)
+                        }
+                    case .goBack:
+                        if let delegate = self.delegate {
+                            delegate.onPrevSong(wsMessage)
+                        }
+                    case .play:
+                        if let delegate = self.delegate {
+                            delegate.onPlay(wsMessage)
+                        }
+                    case .pause:
+                        if let delegate = self.delegate {
+                            delegate.onPause(wsMessage)
+                        }
+                    default:
+                        break;
+                    }
+                    break;
+                case .string(let string):
+                    break;
+                }
+            case .failure(let failure):
+                print("[SharedQSyncManager] websocket data failed: \(failure) (if this is only happening occasionally, you can probably ignore it)")
+            }
+        }
+    }
+    /// Sends a `pauseSong` message to the server
+    public func pauseSong() async throws {
+        if let socket = socket {
+            
+            let jsonData = try JSONEncoder().encode(WSMessage(type: .pause, data: "hi!!!".data(using: .utf8)!, sentAt: Date()))
+                try await socket.send(.data(jsonData))
+        }
+    }
+    /// Sends a `playSong` message to the server
+    public func playSong() async throws {
+        if let socket = socket {
+            let jsonData = try JSONEncoder().encode(WSMessage(type: .play, data: "hi!!!".data(using: .utf8)!, sentAt: Date()))
+            try await socket.send(.data(jsonData))
+        }
+    }
+    /// Sends a `nextSong` message to the server
+    public func nextSong() async throws {
+        if let socket = socket {
+            let jsonData = try JSONEncoder().encode(WSMessage(type: .nextSong, data: "hi!!!".data(using: .utf8)!, sentAt: Date()))
+                try await socket.send(.data(jsonData))
+        }
+    }
+    /// Adds the SQSong to the queue
+    public func addToQueue(song: SQSong, user: SQUser) async throws {
+        if let socket = socket {
+            let jsonData = try JSONEncoder().encode(WSMessage(type: .addToQueue, data: try! JSONEncoder().encode(SQQueueItem(song: song, addedBy: user.username)), sentAt: Date()))
+                try await socket.send(.data(jsonData))
+        }
+    }
+}
---
+++ Sources/SharedQSync/SharedQSyncDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  File.swift
+//  
+//
+//  Created by Payton Curry on 4/6/24.
+//
+
+import Foundation
+
+public protocol SharedQSyncDelegate {
+    /// Called when SharedQSyncManager connects to the group session
+    func onGroupConnect()
+    /// Called when the server sends an updated SQGroup to the client. This can happen for a variety of reasons. Most commonly, it happens when a song is added to the queue, when someone joins the group, or when an action that changes the group's "currentlyPlaying" is used. RECOMMENDED: Replace the current SQGroup with the new one
+    func onGroupUpdate(_ group: SQGroup, _ message: WSMessage)
+    /// Called when a client has skipped to the next song in the queue. RECOMMENDED: Start playing the group's "currentlyPlaying" song, as `onGroupUpdate` is called ~2 seconds prior to this, and has the next song in the queue loaded.
+    func onNextSong(_ message: WSMessage)
+    /// Called when a client has skipped to the previous song in the queue. RECOMMENDED: Start playing the group's "currentlyPlaying" song, as `onGroupUpdate` is called ~2 seconds prior to this, and has the next song in the queue loaded.
+    func onPrevSong(_ message: WSMessage)
+    /// Called when a client plays the song. This function is not a combined play/pause function because of potential desync issues. If you would like to verify playback state, use the SQGroup's "playbackState" object. RECOMMENDED: Play the group's "currentlyPlaying" song.
+    func onPlay(_ message:WSMessage)
+    /// Called when a client pauses the song. This function is not a combined play/pause function because of potential desync issues. If you would like to verify playback state, use the SQGroup's "playbackState" object. RECOMMENDED: Pause the song.
+    func onPause(_ message: WSMessage)
+    /// Called every ~15 seconds as the server sends updated timestamp data to ensure that everybody remains in sync. RECOMMENDED: If the timestamp is more than 2 seconds ahead or behind current playback timestamp, skip to the updated timestamp. Otherwise, do nothing.
+    func onTimestampUpdate(_ timestamp: TimeInterval, _ message: WSMessage)
+    /// Called when a client seeks to a specific part of a song. RECOMMENDED: Seek to the provided timestamp.
+    func onSeekTo(_ timestamp: TimeInterval, _ message: WSMessage)
+}
