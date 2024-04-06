b4cba71b5e2564697045461217b83269e76cfd70
paytondev <81662879+paytontech@users.noreply.github.com>
2024-04-06 15:05:35 -0400

    skip support

--- Tests/SharedQSyncTests/SharedQSyncTests.swift
+++ Tests/SharedQSyncTests/SharedQSyncTests.swift
@@ -1,12 +1,23 @@
 import XCTest
+import OSLog
+import Foundation
 @testable import SharedQSync
 
-final class SharedQSyncTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
+let logger: Logger = Logger(subsystem: "SharedQSync", category: "Tests")
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+@available(macOS 13, *)
+final class SharedQSyncTests: XCTestCase {
+    func testSharedQSync() throws {
+        logger.log("running testSharedQSync")
+        XCTAssertEqual(1 + 2, 3, "basic test")
+        
+        // load the TestData.json file from the Resources folder and decode it into a struct
+        let resourceURL: URL = try XCTUnwrap(Bundle.module.url(forResource: "TestData", withExtension: "json"))
+        let testData = try JSONDecoder().decode(TestData.self, from: Data(contentsOf: resourceURL))
+        XCTAssertEqual("SharedQSync", testData.testModuleName)
     }
+}
+
+struct TestData : Codable, Hashable {
+    var testModuleName: String
 }
