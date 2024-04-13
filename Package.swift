e599ecc9dfb085d883760e8b9b8b9698803d6548
paytondev <81662879+paytontech@users.noreply.github.com>
2024-04-13 16:52:05 -0400

    fix deps

--- Package.swift
+++ Package.swift
@@ -14,20 +14,20 @@ let skipstone = !zero ? [Target.PluginUsage.plugin(name: "skipstone", package: "
 let package = Package(
     name: "sharedqsync",
     defaultLocalization: "en",
-    platforms: [.iOS(.v16), .macOS(.v13), .tvOS(.v16), .watchOS(.v9), .macCatalyst(.v16)],
+    platforms: [.iOS(.v17), .macOS(.v14), .tvOS(.v17), .watchOS(.v10), .macCatalyst(.v17)],
     products: [
         .library(name: "SharedQSync", targets: ["SharedQSync"]),
         .library(name: "SharedQAuth", targets: ["SharedQAuth"]),
     ],
     dependencies: [
-        .package(url: "https://source.skip.tools/skip.git", from: "0.8.28"),
-        .package(url: "https://source.skip.tools/skip-foundation.git", from: "0.0.0"),
-        .package(url: "https://source.skip.tools/skip-firebase.git", from: "0.0.0"),
+//        .package(url: "https://source.skip.tools/skip.git", from: "0.8.28"),
+//        .package(url: "https://source.skip.tools/skip-foundation.git", from: "0.0.0"),
+//        .package(url: "https://source.skip.tools/skip-firebase.git", from: "0.0.0"),
         .package(url: "git@github.com:paytontech/sharedqprotocol.git", branch: "main"),
         .package(url: "https://github.com/apple/swift-docc", branch: "main")
     ],
     targets: [
-        .target(name: "SharedQSync", dependencies: (zero ? [] : [.product(name: "SkipFoundation", package: "skip-foundation"), .product(name: "SharedQProtocol", package: "sharedqprotocol")]), resources: [.process("Resources")], plugins: skipstone),
-        .target(name: "SharedQAuth", dependencies: (zero ? [] : [.product(name: "SkipFoundation", package: "skip-foundation"), .product(name: "SkipFoundation", package: "skip-foundation"),]), resources: [.process("Resources")], plugins: skipstone),
+        .target(name: "SharedQSync", dependencies: [.product(name: "SharedQProtocol", package: "sharedqprotocol")], resources: [.process("Resources")]),
+        .target(name: "SharedQAuth", dependencies: [.product(name: "SharedQProtocol", package: "sharedqprotocol")], resources: [.process("Resources")]),
     ]
 )
