2e60bdc602f39d370cdaa3f090a6536deb477ede
paytondev <81662879+paytontech@users.noreply.github.com>
2024-05-24 15:23:08 -0400

    skip

--- Package.swift
+++ Package.swift
@@ -17,17 +17,15 @@ let package = Package(
     platforms: [.iOS(.v17), .macOS(.v14), .tvOS(.v17), .watchOS(.v10), .macCatalyst(.v17)],
     products: [
         .library(name: "SharedQSync", targets: ["SharedQSync"]),
-        .library(name: "SharedQAuth", targets: ["SharedQAuth"]),
     ],
     dependencies: [
-//        .package(url: "https://source.skip.tools/skip.git", from: "0.8.28"),
-//        .package(url: "https://source.skip.tools/skip-foundation.git", from: "0.0.0"),
-//        .package(url: "https://source.skip.tools/skip-firebase.git", from: "0.0.0"),
+        .package(url: "https://source.skip.tools/skip.git", from: "0.8.28"),
+        .package(url: "https://source.skip.tools/skip-foundation.git", from: "0.0.0"),
+        .package(url: "https://source.skip.tools/skip-firebase.git", from: "0.0.0"),
         .package(url: "git@github.com:paytontech/sharedqprotocol.git", branch: "main"),
         .package(url: "https://github.com/apple/swift-docc", branch: "main")
     ],
     targets: [
-        .target(name: "SharedQSync", dependencies: [.product(name: "SharedQProtocol", package: "sharedqprotocol")], resources: [.process("Resources")]),
-        .target(name: "SharedQAuth", dependencies: [.product(name: "SharedQProtocol", package: "sharedqprotocol")], resources: [.process("Resources")]),
+        .target(name: "SharedQSync", dependencies: (zero ? [.product(name: "SharedQProtocol", package: "sharedqprotocol")] : [.product(name: "SharedQProtocol", package: "sharedqprotocol"), .product(name: "SkipFoundation", package: "skip-foundation")]), resources: [.process("Resources")]),
     ]
 )
